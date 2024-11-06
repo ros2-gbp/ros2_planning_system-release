a259e157567bdcba577ef25789a9f5bb9b6b8530
Francisco Martín Rico <fmrico@gmail.com>
2024-11-06 07:08:28 +0100

    Rebase from 'debian/jazzy/plansys2_planner'

--- test/unit/planner_test.cpp
+++ test/unit/planner_test.cpp
@@ -51,7 +51,7 @@ TEST(planner_expert, generate_plan_good)
   domain_node->set_parameter({"model_file", pkgpath + "/pddl/domain_simple.pddl"});
   problem_node->set_parameter({"model_file", pkgpath + "/pddl/domain_simple.pddl"});
 
-  rclcpp::executors::MultiThreadedExecutor exe(rclcpp::ExecutorOptions(), 8);
+  rclcpp::experimental::executors::EventsExecutor exe;
 
   exe.add_node(domain_node->get_node_base_interface());
   exe.add_node(problem_node->get_node_base_interface());
@@ -130,7 +130,7 @@ TEST(planner_expert, generate_plan_with_domain_constants)
   domain_node->set_parameter({"model_file", pkgpath + "/pddl/domain_simple_constants.pddl"});
   problem_node->set_parameter({"model_file", pkgpath + "/pddl/domain_simple_constants.pddl"});
 
-  rclcpp::executors::MultiThreadedExecutor exe(rclcpp::ExecutorOptions(), 8);
+  rclcpp::experimental::executors::EventsExecutor exe;
 
   exe.add_node(domain_node->get_node_base_interface());
   exe.add_node(problem_node->get_node_base_interface());
