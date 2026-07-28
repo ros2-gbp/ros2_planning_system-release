db6ac3762f7484ad1ba2289e4d23b63a9b5a6840
Francisco Martín Rico <fmrico@gmail.com>
2026-07-28 12:45:56 +0200

    Trimmed the branch to only the plansys2_tools sub directory

--- plansys2_support_py/plansys2_support_py/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-PlanSys2 Support Python Package.
-
-This package provides Python support for PlanSys2 (Planning System 2),
-including client interfaces for domain, problem, planner, and executor
-experts, as well as utilities for PDDL parsing and action execution.
-"""
